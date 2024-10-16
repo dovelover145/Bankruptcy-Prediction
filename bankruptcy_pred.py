410558f37388bbfe35a55b40cf10af54b62beb8d
Ibrahim Siddiqui <ibwaheem12@gmail.com>
2024-10-16 10:17:19 -0700

    Checking for missing values and duplicated rows

---
+++ bankruptcy_pred.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("american_bankruptcy.csv")
+# print(df.head(10))
+
+# DATA PREPROCESSING
+
+# Data cleaning
+
+# missing_values = df.isnull().sum()
+# print(missing_values)
+
+# duplicates = df.duplicated().sum()
+# print(duplicates)
